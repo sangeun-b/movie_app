87f5255c8cc73293c29138c182c8e740f293d512
sangeun-b <you@example.com>
2021-01-23 21:19:41 -0500

    master

---
+++ src/Movie.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__date">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genres">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie_summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
+Movie.prototyps = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
